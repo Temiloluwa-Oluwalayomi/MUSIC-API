9b684f3c967df485e3aca518787f6e8e27f43e7d
CyberLayo <adekoyatemiloluwa02@gmail.com>
2026-03-12 10:24:53 +0100

    fix: update music track details including duration, image, and links

--- index.js
+++ index.js
@@ -19,128 +19,128 @@ const music = [
   {
     Title: 'Time for Shifts',
     Artist: 'Godswill Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '04:22',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e02f2323f6e1f9a1f2f3f4f5f6',
+    Link: 'https://www.google.com/search?q=https://apple.co/godswill-oyor'
   },
   {
     Title: 'Eagles Flight',
     Artist: 'Lawrence Oyor ft. Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '08:34',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/qYJvO0B6D7U/maxresdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://youtube.com/watch%3Fv%3DqYJvO0B6D7U'
   },
   {
     Title: 'Out of My Belly',
     Artist: 'Prospa Ochimana & Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '14:24',
+    Image: 'https://www.google.com/search?q=https://i1.sndcdn.com/artworks-000600123.jpg',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/80123456'
   },
   {
     Title: 'Alignment Chant',
     Artist: 'Lawrence Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '13:31',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e02888c001',
+    Link: 'https://www.google.com/search?q=https://deezer.com/album/150000'
   },
   {
     Title: 'I Will Not Trade',
     Artist: 'Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '10:58',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/theophilus/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://audiomack.com/song/theophilus'
   },
   {
     Title: 'Adua Ke',
     Artist: 'Lawrence & Godswill Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '05:00',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/aduake/maxresdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/160000'
   },
   {
     Title: 'Jugular Jugular',
     Artist: 'Lawrence Oyor ft. Greatman Takit',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '04:36',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/jugular-art',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/295000'
   },
   {
     Title: 'Earthen Vessel',
     Artist: 'Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '20:43',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/theophilus-earthen',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/110000'
   },
   {
     Title: 'Rain of Favour',
     Artist: 'Lawrence & Godswill Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '06:15',
+    Image: 'hhttps://www.google.com/search?q=https://i.ytimg.com/vi/rainfavour/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://apple.co/lawrence-oyor'
   },
   {
     Title: 'Holy Ghost Come',
     Artist: 'Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '15:35',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/hg-come',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/330000'
   },
   {
     Title: 'Stone of Help',
     Artist: 'Godswill Oyor ft. Nathaniel Bassey',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '07:12',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/stonehelp/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://apple.co/stone-of-help'
   },
   {
     Title: 'My Daddy My Daddy',
     Artist: 'Lawrence Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '13:32',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/mydaddy/maxresdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://youtube.com/watch%3Fv%3Dmydaddy'
   },
   {
     Title: 'I Praise You',
-    Artist: 'Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Artist: 'O’Tims ft. Godswill Oyor',
+    Duration: '08:24',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/praise-you',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/440000'
   },
   {
     Title: 'Tongues of Glory',
-    Artist: 'O’Tims ft. Godswill Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Artist: 'Theophilus Sunday',
+    Duration: '03:55',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/tongues/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/550000'
   },
   {
     Title: 'Burn the World',
     Artist: 'Lawrence & Godswill Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
-  },
+    Duration: '09:20',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/burntheworld/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://youtube.com/watch%3Fv%3D8ZAclxmv9xE'
+  },  
   {
     Title: 'Adullam Chants',
     Artist: 'Theophilus Sunday',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '04:33',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/adullam/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/660000'
   },
   {
     Title: 'Joy on My Account',
     Artist: 'Lawrence Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '09:07',
+    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/joy-account',
+    Link: 'https://www.google.com/search?q=https://deezer.com/track/770000'
   },
   {
     Title: 'I Have Escaped',
     Artist: 'Lawrence Oyor',
-    Duration: '13:40',
-    Image: 'https://www.google.com/search?q=https://i.scdn.co/image/ab67616d00001e021a361c77935790a8a6663f70',
-    Link: 'https://www.google.com/search?q=https://deezer.com/track/1018805432'
+    Duration: '05:45',
+    Image: 'https://www.google.com/search?q=https://i.ytimg.com/vi/escaped/hqdefault.jpg',
+    Link: 'https://www.google.com/search?q=https://apple.co/escaped'
   }
 ]
 
